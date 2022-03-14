0694f244009f4945e642950ae0d01e0ddd959f43
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-03-14 18:39:21 +0300

    5 test

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -1,4 +1,17 @@
 package ru.netology.sqr;
 
 public class SQRService {
+
+    public int sqrtErect(int num1, int num2) {
+        int count = 0;
+        for (int i = 10; i < 99; i++) {
+            if (num1 < i*i) {
+                if (i*i < num2) {
+                    count=count+1;
+                }
+            }
+        }
+        System.out.println("В грацицу попало " + count + " чисел");
+        return count;
+    }
 }
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -1,7 +1,24 @@
 package ru.netology.sqr;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"'OneHundred',100,200,4",
+            "'TwoHundred',100,300,7",
+            "'MaximumRange',90,10000,89",
+            "'MinimumRange',90,110,1",
+            "'Zero',90,100,0" })
+    void shouldNumberSqrt(String name, int num1, int num2, int expected) {
+        SQRService sqrService = new SQRService();
+        int actual = sqrService.sqrtErect(num1,num2);
+        assertEquals(expected, actual);
+    }
+
 
 }
