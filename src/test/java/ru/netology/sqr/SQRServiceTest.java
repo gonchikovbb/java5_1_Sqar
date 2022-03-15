1c987f9d08a9d29b24800a3b8f4b3c49030e39f5
Bolot Gonchikov <gonchikovbb@gmail.com>
2022-03-15 13:11:08 +0300

    add UTF-8, borders included, merged if

--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -9,11 +9,11 @@ import static org.junit.jupiter.api.Assertions.*;
 class SQRServiceTest {
 
     @ParameterizedTest
-    @CsvSource(value = {"'OneHundred',100,200,4",
-            "'TwoHundred',100,300,7",
+    @CsvSource(value = {"'OneHundred',100,200,5",
+            "'TwoHundred',100,300,8",
             "'MaximumRange',90,10000,89",
             "'MinimumRange',90,110,1",
-            "'Zero',90,100,0"})
+            "'Zero',90,99,0"})
     void shouldNumberSqrt(String name, int num1, int num2, int expected) {
         SQRService sqrService = new SQRService();
         int actual = sqrService.sqrtErect(num1, num2);
